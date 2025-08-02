07e60f41acac97c3494c95a34af6ecfa83077299
Kakaire Steven <kakairestevenking@gmail.com>
2025-08-02 19:30:37 +0300

    Remove number counts (e.g 01- ) from Parish names

--- Elections/2021/Presidential/parse2021.py
+++ Elections/2021/Presidential/parse2021.py
@@ -1,6 +1,9 @@
 # edit string if it starts with 2-digits and '-'
 def editStations(df):
     df.station = df.station.apply(lambda x: x[3:]
-                                  if isinstance(x[:2], int)
+
+                                  # Facing errors with if-else statement.
+                                  # if isinstance(x[:2], int) else str(x)
+                                 )
     return df
 
